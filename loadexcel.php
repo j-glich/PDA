ca1d1880b8d12cd76ff7a33efefff1401aacda9c
j-glich <72561315+j-glich@users.noreply.github.com>
2022-06-18 14:39:41 -0500

    Configuracion de reporte

--- loadexcel.php
+++ loadexcel.php
@@ -20,7 +20,10 @@ $datos = execQuery($sql);
 if($datos == 0){
  echo "<script>window.location.replace('view/index.php?sp_registro=4')</script>";
 }else{
-
+    $CellA = 'A';
+    $CellB = 'B';
+    $CellD = 'D';
+    $CellF = 'F';
     $documento = new Spreadsheet();
     $documento
         ->getProperties()
@@ -43,11 +46,48 @@ if($datos == 0){
     $sheet->getStyle('D4')->getAlignment()->setHorizontal('center');
     $sheet->setCellValue('K4',$hoy);
     $sheet->setCellValue('G5',$datos[0]['DO_NOMBRE_1']);
-    
-    
-    
-    
-    
+
+
+    for ($i=0; $i < sizeof($datos) ; $i++) { 
+      $array_categorias[] = array('clave' =>$datos[$i]['CAT_CVE'],'titulo' =>$datos[$i]['CAT_TITULO']);
+      $array_subcategorias[] = array('clave' =>$datos[$i]['SCAT_CVE'],'titulo' =>$datos[$i]['SCAT_TITULO']);
+      $array_productos[] = array('titulo' =>$datos[$i]['PR_TITULO'],'desc' =>$datos[$i]['PR_DESCRIPCION'],'cve_subCat' =>$datos[$i]['PR_SCAT_CVE']);
+    }
+    $array_categorias =  array_merge(array_unique($array_categorias, SORT_REGULAR));
+    $array_subcategorias = array_merge(array_unique($array_subcategorias, SORT_REGULAR));
+
+
+    $count_productos = 0;
+    $count_subCat = 0;
+
+    for ($i=0; $i < sizeof($array_productos) ; $i++) { 
+      $sheet->setCellValue($CellD.(9+$i),$array_productos[$i]['titulo'])->getStyle($CellD.(9+$i))->getAlignment()->setWrapText(true);
+      $sheet->setCellValue($CellF.(9+$i),$array_productos[$i]['desc'])->getStyle($CellF.(9+$i))->getAlignment()->setWrapText(true);
+      $sheet->getRowDimension((9+$i))->setRowHeight(30,'px');
+    }
+
+    foreach( $array_subcategorias as $row){   
+      $count = array_count_values(array_column($array_productos,'cve_subCat'))[$row['clave']];
+      $array_contadores[] = array(
+        'clave'=>$row['clave'],
+        'titulo'=>$row['titulo'],
+        'count'=>$count);
+    } 
+    $suma =0;
+    for ($i=0; $i <sizeof($array_contadores) ; $i++) { 
+      if($i==0){
+        $suma = (8+$array_contadores[0]['count']);
+        $sheet->mergeCells('B9:B'.$suma)
+        ->setCellValue('B9',$array_contadores[0]['titulo'])
+        ->getStyle('B9:B'.$suma)->getAlignment()->setHorizontal('center');
+      }else{
+        $suma2 = ($suma+$array_contadores[$i]['count']);
+        $sheet->mergeCells('B'.($suma+1).':B'.$suma2)
+        ->setCellValue('B'.($suma+1),$array_contadores[$i]['titulo'])
+        ->getStyle('B'.($suma+1).':B'.$suma2)->getAlignment()->setHorizontal('center');
+        $suma = $suma2;
+      }
+    }
     //  Los siguientes encabezados son necesarios para que
      // el navegador entienda que no le estamos mandando
      // simple HTML
