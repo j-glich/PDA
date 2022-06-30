1851422502d82dae7800eeb352a517b4a25b4259
j-glich <72561315+j-glich@users.noreply.github.com>
2022-06-29 22:25:06 -0500

    Actulizacion

---
+++ enviarEmail.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/'
+$mail = new PHPMailer(true);
+
+try {
+                              //code...
+} catch (\Throwable $th) {
+                              //throw $th;
+}
